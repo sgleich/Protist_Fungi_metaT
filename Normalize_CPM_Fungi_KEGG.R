fcff126df5f3dfecd411ef40e794e8764c040aab
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2024-05-11 19:52:57 -0400

    Update Normalize_CPM_Fungi_KEGG.R

--- Normalize_CPM_Fungi_KEGG.R
+++ Normalize_CPM_Fungi_KEGG.R
@@ -1,12 +1,13 @@
-### Protist + Fungi MetaT - Library Normalization and CPM Calculation ###
-### This script will normalize and calculate the CPM of the decomposition experiment metaT data for Fungi ONLY to use in downstream KEGG KO analyses ###
-### Updated: February 28, 2024 ###
+### Protist + Fungi MetaT - Library Normalization and CPM Calculation Fungi ###
+### This script will normalize and calculate the CPM of the decomposition experiment metaT data to use in downstream analyses. These CPM values will be used for fungal-specific analyses ###
+### Updated: May 11, 2024 ###
+
 
 # Set working directory
-setwd("~/Desktop/PARAGON_DATA_FINAL")
+setwd("~/Desktop/PARAGON_DATA_FINAL/FINAL")
 
 # Load in taxonomic IDs (dfEuk), functional IDs (dfEgg), and transcript abundances (dfSalmon)
-dfEuk <- read.delim("eukulele_tax_myco.annotations",header=TRUE,sep="\t")
+dfEuk <- read.delim("eukulele.out",header=TRUE,sep="\t")
 dfEgg <- read.delim("eggnog_func.annotations",header=TRUE,skip=3)
 dfSalmon <- read.csv("salmon_wide.csv",header=TRUE,row.names=1)
 
@@ -21,7 +22,6 @@ colnames(dfEgg) <- c("transcript_name","KEGG","CAZy")
 
 # Join EUKulele and eggnog-mapper tables and modify names of proteinIDs
 dfEukEgg <- left_join(dfEuk,dfEgg)
-dfEukEgg <- subset(dfEukEgg,grepl(".p1",dfEukEgg$transcript_name))
 colz <- colsplit(dfEukEgg$transcript_name,"\\.p",c("name","p"))
 dfEukEgg$transcript_name <- colz$name
 colnames(dfSalmon)[1] <- "transcript_name"
@@ -30,28 +30,26 @@ colnames(dfSalmon)[1] <- "transcript_name"
 dfAll <- left_join(dfEukEgg,dfSalmon)
 
 # Grab all eukaryotic proteinIDs - we don't care about anything that is not eukaryotic.
-dfAll <- subset(dfAll,grepl("Eukaryot",dfAll$full_classification))
+dfAll <- subset(dfAll,grepl("Fungi",dfAll$full_classification))
 
 # Parse KEGG ko data.
 dfAll$KEGG <- ifelse(is.na(dfAll$KEGG),"",dfAll$KEGG)
 dfAll$CAZy <- ifelse(is.na(dfAll$CAZy),"",dfAll$CAZy)
-dfAll$CAZy <- NULL
 dfAll$KEGG <- str_remove_all(dfAll$KEGG,"ko:")
 dfAll$KEGG <- ifelse(is.na(dfAll$KEGG),"",dfAll$KEGG)
 dfAll$KEGG <- str_split(dfAll$KEGG,",")
+dfAll$CAZy <- str_split(dfAll$CAZy,",")
 dfWide <- unnest(dfAll,KEGG)
-colnames(dfWide)[1:3] <- c("Name","Taxonomy","KEGG")
+dfWide <- unnest(dfWide,CAZy)
+colnames(dfWide)[1:4] <- c("Name","Taxonomy","KEGG","CAZy")
 dfWide  <- dfWide %>% distinct(.,.keep_all = TRUE) %>% as.data.frame()
 
-# Subset for fungi
-dfWide <- subset(dfWide,grepl("Fungi",dfWide$Taxonomy))
-
 # Separate Exp #1 and Exp #2 from Exp #3 (Experiment #3 was sequenced separately)
-dfExp12 <- dfWide[,c("Name","Taxonomy","KEGG","RotT0_Exp1","RotT3_Exp1","RotT6_Exp1","RotT0_Exp2","RotT3_Exp2","RotT6_Exp2","WaterColumn1_Exp1","WaterColumn2_Exp1","WaterColumn1_Exp2","WaterColumn2_Exp2")]
-dfExp3 <- dfWide[,c("Name","Taxonomy","KEGG","RotT0_Exp3","RotT3_Exp3","RotT6_Exp3","WaterColumn1_Exp3","WaterColumn2_Exp3")] 
+dfExp12 <- dfWide[,c("Name","Taxonomy","KEGG","CAZy","RotT0_Exp1","RotT3_Exp1","RotT6_Exp1","RotT0_Exp2","RotT3_Exp2","RotT6_Exp2","WaterColumn1_Exp1","WaterColumn2_Exp1","WaterColumn1_Exp2","WaterColumn2_Exp2")]
+dfExp3 <- dfWide[,c("Name","Taxonomy","KEGG","CAZy","RotT0_Exp3","RotT3_Exp3","RotT6_Exp3","WaterColumn1_Exp3","WaterColumn2_Exp3")] 
 
 # Normalize Exp 1&2 Libraries and Calculate CPM
-dgeExp12_list <- DGEList(counts=dfExp12[4:13],genes=dfExp12[1:3],group=c(rep("T0_Exp1",1),rep("T3_Exp1",1),rep("T6_Exp1",1),rep("T0_Exp2",1),rep("T3_Exp2",1),rep("T6_Exp2",1),rep("Water_Exp1",2),rep("Water_Exp2",2)))
+dgeExp12_list <- DGEList(counts=dfExp12[5:14],genes=dfExp12[1:4],group=c(rep("T0_Exp1",1),rep("T3_Exp1",1),rep("T6_Exp1",1),rep("T0_Exp2",1),rep("T3_Exp2",1),rep("T6_Exp2",1),rep("Water_Exp1",2),rep("Water_Exp2",2)))
 dgeExp12_list$samples # Visualize groups
 
 Exp12Norm <- calcNormFactors(dgeExp12_list,method = "TMM")
@@ -60,7 +58,7 @@ Exp12Cpm <-as.data.frame(Exp12Cpm)
 Exp12Cpm <- data.frame(Exp12Norm$genes,Exp12Cpm)
 
 # Normalize Exp 3 Libraries and Calculate CPM 
-dgeExp3 <- DGEList(counts=dfExp3[4:8],genes=dfExp3[1:3],group=c(rep("T0_Exp3",1),rep("T3_Exp3",1),rep("T6_Exp3",1),rep("Water_Exp3",2)))
+dgeExp3 <- DGEList(counts=dfExp3[5:9],genes=dfExp3[1:4],group=c(rep("T0_Exp3",1),rep("T3_Exp3",1),rep("T6_Exp3",1),rep("Water_Exp3",2)))
 dgeExp3$samples # Visualize groups
 
 Exp3Norm <- calcNormFactors(dgeExp3,method = "TMM")
@@ -71,5 +69,6 @@ Exp3Cpm <-data.frame(Exp3Norm$genes,Exp3Cpm)
 # Combine Normalized Exp 1&2 data with Normalized Exp 3 data
 dfFin <- left_join(Exp12Cpm,Exp3Cpm)
 
+
 # Save CPM values for downstream analyses
-write.csv(dfFin,"CPM_Fungi_KEGG.csv")
+write.csv(dfFin,"CPM_Fungi.csv")
